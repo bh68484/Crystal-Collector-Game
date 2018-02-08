67a004746313102eeadea5e5938761c4e36d9999
Brandon Harrington <bh68484@gmail.com>
2018-02-08 16:04:21 -0500

    Updated JS files with working adding counter.  Needs to write to HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    console.log( "ready!" );
+    console.log("ready!");
 
 
 var randomTargetNumber;
@@ -11,25 +11,75 @@ var crystalFourNumber;
 //Counters
 var winCount = 0;
 var lossCount = 0;
-var crystalTotalCount;
+var crystalTotalCount = 0;
 
 function startGame() {
+    //Generates random game number
     randomTargetNumber = Math.floor(Math.random() * 100) + 30;
     console.log("Random Number", randomTargetNumber);
+   
     //Appends randomTargetNumber to the crystal counter
     $("#crystalTarget").html(randomTargetNumber);
+   
+    //Generates random number for Crystal One
     crystalOneNumber = Math.floor(Math.random() * 3) + 1;
-    console.log("Crystal One Number", crystalOneNumber);
+    console.log("Crystal One Number", crystalOneNumber); 
+    // $("#crystalOne").attr("value", crystalOneNumber);
+   
+    
+    //Generates random number for Crystal Two
     crystalTwoNumber = Math.floor(Math.random() * 8) + 1;
     console.log("Crystal Two Number", crystalTwoNumber);
+    
+    //Generates random number for Crystal Three
     crystalThreeNumber = Math.floor(Math.random() * 11) + 1;
     console.log("Crystal Three Number", crystalThreeNumber);
+    
+    //Generates random Number for Crystal Four
     crystalFourNumber = Math.floor(Math.random() * 12) + 1;
     console.log("Crystal Four Number", crystalFourNumber);
 }
+//this.val - look into
+//Checks to see if game has been won or lost
+var winCheck = function() {
+    if (crystalTotalCount == randomTargetNumber) {
+        wins++;
+        alert("Hooray! You won!");
+        $("#wins").html(winCount);
+        startGame();
 
+    } else if (crystalTotalCount > randomTargetNumber) {
+        losses++;
+        alert("You Lose!");
+        $("#losses").html(lossCount);
+        startGame();
+    }
+}
+console.log(winCheck);
+//Setting up button clicks
 
+$(document).ready(function() { 
+    $('.crystalOne').click(function() { 
+        crystalTotalCount = crystalOneNumber + crystalTotalCount;
+        alert(crystalTotalCount);
+    }); 
+    $('.crystalTwo').click(function() { 
+        crystalTotalCount = crystalTwoNumber + crystalTotalCount;
+        alert(crystalTotalCount);
+    }); 
+    $('.crystalThree').click(function() { 
+        crystalTotalCount = crystalTwoNumber + crystalTotalCount;
+        alert(crystalTotalCount);
+    }); 
+    $('.crystalFour').click(function() { 
+        crystalTotalCount = crystalFourNumber + crystalTotalCount;
+        alert(crystalTotalCount);
+    }); 
+}); 
 
 //Main Process
 startGame();
+
+
+
 });

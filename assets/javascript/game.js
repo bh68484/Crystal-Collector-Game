b5f50b1b4c8717f73613ed6afbfabc25ea6a325b
Brandon Harrington <bh68484@gmail.com>
2018-02-08 18:33:59 -0500

    updates - working counter and buttons with random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -38,44 +38,57 @@ function startGame() {
     //Generates random Number for Crystal Four
     crystalFourNumber = Math.floor(Math.random() * 12) + 1;
     console.log("Crystal Four Number", crystalFourNumber);
+    
 }
 //this.val - look into
 //Checks to see if game has been won or lost
-var winCheck = function() {
-    if (crystalTotalCount == randomTargetNumber) {
-        wins++;
-        alert("Hooray! You won!");
-        $("#wins").html(winCount);
-        startGame();
+// 
 
-    } else if (crystalTotalCount > randomTargetNumber) {
-        losses++;
-        alert("You Lose!");
-        $("#losses").html(lossCount);
-        startGame();
-    }
-}
-console.log(winCheck);
 //Setting up button clicks
 
 $(document).ready(function() { 
     $('.crystalOne').click(function() { 
         crystalTotalCount = crystalOneNumber + crystalTotalCount;
         alert(crystalTotalCount);
+        $("#crystalTotalCount").html(crystalTotalCount);
+        console.log("crystal counters", crystalTotalCount);
     }); 
     $('.crystalTwo').click(function() { 
         crystalTotalCount = crystalTwoNumber + crystalTotalCount;
         alert(crystalTotalCount);
+        $("#crystalTotalCount").html(crystalTotalCount);
+        console.log("crystal counters", crystalTotalCount);
+
     }); 
     $('.crystalThree').click(function() { 
-        crystalTotalCount = crystalTwoNumber + crystalTotalCount;
+        crystalTotalCount = crystalThreeNumber + crystalTotalCount;
         alert(crystalTotalCount);
+        $("#crystalTotalCount").html(crystalTotalCount);
+        console.log("crystal counters", crystalTotalCount);
     }); 
     $('.crystalFour').click(function() { 
         crystalTotalCount = crystalFourNumber + crystalTotalCount;
         alert(crystalTotalCount);
+        $("#crystalTotalCount").html(crystalTotalCount);
+        console.log("crystal counters", crystalTotalCount);
     }); 
 }); 
+//old code - maybe revisit
+// var winCheck = function() {
+    //     if (crystalTotalCount == randomTargetNumber) {
+    //         wins++;
+    //         alert("Hooray! You won!");
+    //         $("#wins").html(winCount);
+    //         startGame();
+    
+    //     } else if (crystalTotalCount > randomTargetNumber) {
+    //         losses++;
+    //         alert("You Lose!");
+    //         $("#losses").html(lossCount);
+    //         startGame();
+    //     }
+    // }
+
 
 //Main Process
 startGame();
